12fef1697099d170c7d611d33b5d3d1d46842f80
Grace Murphy <localghost385@gmail.com>
2022-12-16 22:58:47 +0000

    v0.0.3 Added development branch did work on scroll animation.

--- script.js
+++ script.js
@@ -4,13 +4,23 @@
  * - CHANE FAVICONS TO REPRESENT THEME 
  * - CREATE DARK VARIANT OF VECTOR 
  * - SCROLL ANIMATION 
-*/
+ */
 
 //---------------[ LIGHT / DARK MODE ]---------------{{{
 
 
 //---}}}---
 
-//----------------[ SSCROLL ANIMATION ]----------------{{{ 
+//----------------[ SCROLL ANIMATION ]----------------{{{ 
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        entry.target.classList.toggle('shown', entry.isIntersecting);
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
 
 //---}}}---
